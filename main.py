20346430fa513cf3bdf71793152209ce9c44c7e7
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2022-02-19 15:14:03 +0000

    train should now be able to run from main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import argparse
+from lib.Train import Train
+from lib.Inputs import Read_Input, Create_Parser
+import torch
 
 if __name__ == '__main__':
-    pass
+
+    path_data = 'data'
+    input_data = Read_Input('inputs.yaml')
+
+    torch.manual_seed(0)
+
+    Train(path_data, input_data)
