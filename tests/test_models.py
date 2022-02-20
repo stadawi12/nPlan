2a8c80965488291685078938fd4b41a8e2e3f115
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2022-02-20 18:34:59 +0000

    Added some extra unit tests for testing the convnet model

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,24 +9,45 @@ import linear
 
 class TestModels(unittest.TestCase):
 
-    model = linear.Linear()
 
     def test_output_shape(self):
+        import linear
+        model = linear.Linear()
         # generate a random input sample
         inpt = torch.randn(2, 50)
-        out  = self.model(inpt)
+        out  = model(inpt)
 
         self.assertEqual(out.shape, torch.Size([2, 121]))
 
     def test_output_range(self):
+        import linear
+        model = linear.Linear()
         # generate a random input sample and ensure the output is
         # between the range 0 and 1
         inpt = torch.randn(100, 50)
-        out  = self.model(inpt)
+        out  = model(inpt)
         self.assertTrue(out.max() < 1)
         self.assertTrue(out.min() > 0)
 
-        # self.assertTrue(out.max())
+    def test_output_shape_conv(self):
+        import convNet
+        model = convNet.UNet()
+        # generate a random input sample
+        inpt = torch.randn(2, 50)
+        out  = model(inpt)
+
+        self.assertEqual(out.shape, torch.Size([2, 121]))
+
+    def test_output_range_conv(self):
+        import convNet
+        model = convNet.UNet()
+        # generate a random input sample and ensure the output is
+        # between the range 0 and 1
+        inpt = torch.randn(2, 50)
+        out  = model(inpt)
+        self.assertTrue(out.max() < 1)
+        self.assertTrue(out.min() > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
