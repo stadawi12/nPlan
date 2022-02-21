d66ccfc776e41d939853abf976466fbe7143a3df
Dawid Stasiak <mr.dawidstasiak@outlook.com>
2022-02-21 17:42:31 +0000

    made using seed functional now, you can decide whether to use seed from inputs file

--- main.py
+++ main.py
@@ -10,12 +10,19 @@ import torch
 if __name__ == '__main__':
 
     args = Create_Parser()
+    input_data = Read_Input('inputs.yaml')
+
+    use_seed: bool = input_data["use_seed"]
+    seed: int      = input_data["seed"]
 
     if args.action == 'train':
 
         path_data = 'data'
         input_data = Read_Input('inputs.yaml')
 
-        torch.manual_seed(0)
+        
+        if use_seed:
+            torch.manual_seed(seed)
+            print("Using seed: ", seed)
 
         Train(path_data, input_data)
